b89716aa49bed91b5eb86fea5790905a63280516
24HP0081 24HP0081 <24HP0081@admins-iMac.local>
2024-12-17 18:36:09 +0800

    push

--- auth-middleware.js
+++ auth-middleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const auth = req.headers["authorization"];
+  const auth = req.headers["Authorization"];
   const token = auth.split(" ")[1];
   if (!token) res.json({ message: "no token in header" });
 
